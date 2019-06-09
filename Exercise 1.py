b5dcbb67553a77d3088e62b2cd138a48288bae6c
amirj1984 <amjodeiri@yahoo.com>
2019-06-09 22:56:47 +0430

    This is the 1st exercise!

---
+++ Exercise 1.py
@@ -0,0 +1,24 @@
+first_num = 10
+x = 1.2
+print(x+first_num)
+print(x*first_num)
+print(type(x))
+print(type(x+first_num))
+print(type(x*first_num))
+print(type(first_num))
+print("hello Pyton world!")
+message = "hello Pyton world!"
+print(message)
+print(message)
+name = "amir mohammad jodeiri khoshbaf"
+print(name.title())
+print(name.upper())
+print(name.lower())
+first_name = "amir mohammad"
+last_name = "jodeiri khoshbaf"
+full_name = first_name+" "+last_name
+print(full_name)
+print(full_name.title())
+print("Hello, "+full_name.title()+"!")
+
+
