eea7f5949964ee97d8b78765f93a0d64eac06976
amirj1984 <amjodeiri@yahoo.com>
2019-06-23 14:49:25 +0430

    # this code reads a positive integer number greater than 1 and indicates if it is a prime number or not (for loop,  range, break)

--- S06/prime_for_loop.py
+++ S06/prime_for_loop.py
@@ -1,10 +1,10 @@
 # this code reads a positive integer number greater than 1 and indicates if it is a prime number or not (for loop and range)
 
 num = int(input("enter a positive integer number greater than 1 : "))
-sqr_root_num = int(num ** 0.5 + 1)
 is_prime = True
-for i in range(2, sqr_root_num):
+for i in range(2, num):
     if num % i == 0:
         is_prime = False
+        break
 
 print("is it a prime number", is_prime)
