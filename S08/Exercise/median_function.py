63519f5f01d870fd7996ec713d5826206cd27935
amirj1984 <amjodeiri@yahoo.com>
2019-06-29 21:20:03 +0430

    # this function takes a list of integers and returns its median

---
+++ S08/Exercise/median_function.py
@@ -0,0 +1,10 @@
+# this function takes a list of integers and returns its median
+
+def median(*numbers):
+    n = len(numbers)
+    if n % 2 == 1:
+        return sorted(numbers)[n//2]
+    elif n % 2 == 0:
+        return (sorted(numbers)[int(n/2 - 1)] + sorted(numbers)[int(n/2)])/2
+
+print(median(1))
