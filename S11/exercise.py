bf69ce958fcaf68a5810d76e3654fd05536234f3
amirj1984 <amjodeiri@yahoo.com>
2019-07-09 12:45:04 +0430

    exercise

--- S11/exercise.py
+++ S11/exercise.py
@@ -38,3 +38,48 @@ age = 35.973
 # print(f"({name:<15},{height:^+10.2f},{age:>+15.2f})")
 
 
+# class Tree:
+#     is_something = True
+#     name = "derakht"
+#
+#     def get_height_of_tree(tree):
+#         return tree.height
+#
+#
+# tree_1 = Tree()
+# tree_1.name = "chenar"
+# tree_1.height = 12
+#
+# tree_2 = Tree()
+# tree_2.name = "sarv"
+# tree_2.height = 10
+#
+# tree_3 = Tree()
+# tree_3.age = 55
+# tree_3.height = 14
+
+# for tree in [tree_1, tree_2, tree_3]:
+#     # print(tree.name, tree.is_something, tree.height)
+#     print(tree.__dict__)
+# print(Tree.__dict__)
+
+# res = get_height_of_tree(tree_1)
+# print(res)
+
+# res = Tree.get_height_of_tree(tree_1)
+# print(res)
+
+# print(tree_1.get_height_of_tree())
+
+class Tree:
+    def __init__(self, a, c, h):
+        self.age = a
+        self.color = c
+        self.height = h
+
+
+tree_1 = Tree(77, "green", 12)
+tree_2 = Tree(90, "Brown", 21)
+
+for tree in [tree_1, tree_2]:
+    print(tree.__dict__)
