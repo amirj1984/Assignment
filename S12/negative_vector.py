6d35e5e707dc8506d5e571333716956aa00e1d4e
amirj1984 <amjodeiri@yahoo.com>
2019-07-10 18:09:25 +0430

    # this program returns the negative vector of a given one using class vector

---
+++ S12/negative_vector.py
@@ -0,0 +1,16 @@
+# this program returns the negative vector of a given one using class vector
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __neg__(self):
+        return Vector(-self.x, -self.y)
+
+
+vec1 = Vector(3, 4)
+vec2 = vec1.__neg__()
+# vec2 = -vec1
+print(vec2.x, vec2.y)
+
