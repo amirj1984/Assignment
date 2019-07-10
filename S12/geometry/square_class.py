b57e5c527f3426e37c94db94414b4ad566d4b827
amirj1984 <amjodeiri@yahoo.com>
2019-07-10 20:24:31 +0430

    # this program returns area & perimeter of a square using class square and docstring test # Question: while using docstring test, it receives failure, why?

--- S12/geometry/square_class.py
+++ S12/geometry/square_class.py
@@ -1,17 +1,31 @@
-# this program returns area and perimeter of a square using class square
+# this program returns area & perimeter of a square using class square and docstring test
+# Question: while using docstring test, it receives failure, why?
 
 
 class Square:
-    def __init__(self, side):
+    """
+    EXAMPLE
+    -------
+    >>> sq = Square(3)
+    >>> sq.area_of_square()
+    9.0
+    >>> sq.perimeter_of_square()
+    12.0
+    """
+    def __init__(self, side: float):
         self.side = side
 
-    def area_of_square(self):
+    def area_of_square(self) -> float:
         return self.side ** 2
 
-    def perimeter_of_square(self):
+    def perimeter_of_square(self) -> float:
         return self.side * 4
 
 
-sq = Square(5)
+sq = Square(2.5)
 print("area of the square is: ", sq.area_of_square(), "\n", "perimeter of the square is: ", sq.perimeter_of_square())
 
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
